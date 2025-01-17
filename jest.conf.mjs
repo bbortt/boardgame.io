8ae778bf269947506e3ca6a43bbdba280efeb837
Timon Borter <timon.borter@gmx.ch>
2025-01-17 23:17:26 +0100

    ci(tests): extracted jest config from package.json

---
+++ jest.conf.mjs
@@ -0,0 +1,32 @@
+/** @type {import('jest').Config} */
+export default {
+  preset: 'ts-jest/presets/js-with-babel',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css)$': 'identity-obj-proxy',
+    '\\.(svg)$': '<rootDir>/.empty_module.js',
+    'svelte-json-tree-auto':
+      '<rootDir>/src/client/debug/tests/JSONTree.mock.svelte',
+  },
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    'src/client/transport/dummy',
+    'src/client/debug/.*',
+    'src/types.ts',
+  ],
+  setupFiles: ['raf/polyfill', 'jest-date-mock'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transform: {
+    '^.+\\.svelte$': 'jest-transform-svelte',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(boardgame.io|flatted|svelte-icons)/)',
+  ],
+  testPathIgnorePatterns: [
+    ".husky/",
+    ".npm/",
+    "examples/",
+    "integration/",
+    "node_modules/"
+  ],
+};
