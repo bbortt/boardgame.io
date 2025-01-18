3c08a7160acdf93c3123fc6cf82743b4c7c92c17
Timon Borter <timon.borter@gmx.ch>
2025-01-18 09:18:20 +0100

    ci(jest): updated babel and jest configuration

--- .jest.config.mjs
+++ .jest.config.mjs
@@ -1,10 +1,19 @@
-/** @type {import('jest').Config} */
-export default {
-  preset: 'ts-jest/presets/js-with-babel',
+import { createJsWithBabelPreset } from 'ts-jest';
+
+const { CI } = process.env;
+
+// https://kulshekhar.github.io/ts-jest/docs/getting-started/presets/#createjswithbabelesmpresetoptions
+const presetConfig = createJsWithBabelPreset({
+  babelConfig: '<rootDir>/babel.config.json',
+});
+
+/** @type { import('ts-jest').JestConfigWithTsJest } */
+const jestConfig = {
+  ...presetConfig,
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '\\.(css)$': 'identity-obj-proxy',
-    '\\.(svg)$': '<rootDir>/.empty_module.js',
+    '\\.(svg)$': '<rootDir>/.empty_module.mjs',
     'svelte-json-tree-auto':
       '<rootDir>/src/client/debug/tests/JSONTree.mock.svelte',
   },
@@ -14,19 +23,27 @@ export default {
     'src/client/debug/.*',
     'src/types.ts',
   ],
+  reporters: [
+    ...(CI ? [['github-actions', { silent: false }], 'summary'] : ['default']),
+    ['jest-junit', { outputDirectory: 'reports', outputName: 'report.xml' }],
+  ],
   setupFiles: ['raf/polyfill', 'jest-date-mock'],
   setupFilesAfterEnv: ['<rootDir>/.jest.setup.js'],
   transform: {
     '^.+\\.svelte$': 'jest-transform-svelte',
+    ...presetConfig.transform,
   },
   transformIgnorePatterns: [
+    '<rootDir>/.babelrc.mjs',
     'node_modules/(?!(boardgame.io|flatted|svelte-icons)/)',
   ],
   testPathIgnorePatterns: [
-    ".husky/",
-    ".npm/",
-    "examples/",
-    "integration/",
-    "node_modules/"
+    '.husky/',
+    '.npm/',
+    'examples/',
+    'integration/',
+    'node_modules/',
   ],
 };
+
+export default jestConfig;
---
+++ .lintstagedrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  '{,{.github,benchmark,examples,packages,src}/**/}*.{css,(m|)js(x|),json,md,ts(x|)}':
+    ['prettier --write'],
+};
